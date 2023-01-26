452ff87b337e9e9fd8d2742384e50a1d99d1bf00
johnboyce <John_Boyce@comcast.com>
2023-01-25 19:45:08 -0500

    initial

---
+++ src/main/java/com/comcast/oktest/OktestApplication.java
@@ -0,0 +1,13 @@
+package com.comcast.oktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comcast/oktest/OktestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comcast.oktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
