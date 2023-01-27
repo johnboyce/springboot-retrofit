091cad6fd8645444b10470fc532d155a47b9c605
johnboyce <John_Boyce@comcast.com>
2023-01-27 01:00:39 -0500

    Add some configuration to OkHttp

--- src/main/java/com/comcast/oktest/config/AppConfig.java
+++ src/main/java/com/comcast/oktest/config/AppConfig.java
@@ -1,11 +1,14 @@
 package com.comcast.oktest.config;
 
+import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class AppConfig {
@@ -14,6 +17,10 @@ public class AppConfig {
 
     public OkHttpClient.Builder okHttpClientBuilder() {
         return new OkHttpClient.Builder()
+                .connectTimeout(2, TimeUnit.SECONDS)
+                .readTimeout(5, TimeUnit.SECONDS)
+                .writeTimeout(5, TimeUnit.SECONDS)
+                .connectionPool(new ConnectionPool(3, 10, TimeUnit.NANOSECONDS))
                 .addInterceptor(new HttpLoggingInterceptor()
                         .setLevel(HttpLoggingInterceptor.Level.BASIC));
     }
