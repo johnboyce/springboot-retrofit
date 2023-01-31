57c604d926fa29f7391d818059001a68574a01e8
johnboyce <johnboyce@comcast.net>
2023-01-30 19:09:23 -0500

    Externalize server endpoints

--- src/main/java/com/johnnyb/oktest/config/AppConfig.java
+++ src/main/java/com/johnnyb/oktest/config/AppConfig.java
@@ -2,13 +2,16 @@ package com.johnnyb.oktest.config;
 
 import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
+
 import okhttp3.logging.HttpLoggingInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -27,9 +30,9 @@ public class AppConfig {
     }
 
     @Bean
-    public Retrofit userRetrofit() {
+    public Retrofit userRetrofit(@Value("${endpoint.user}") String endpointUser) {
         return new Retrofit.Builder()
-                .baseUrl("https://reqres.in/")
+                .baseUrl(endpointUser)
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(okHttpClientBuilder().build())
@@ -37,9 +40,9 @@ public class AppConfig {
     }
 
     @Bean
-    public Retrofit productRetrofit() {
+    public Retrofit productRetrofit(@Value("${endpoint.product}") String endpointProduct) {
         return new Retrofit.Builder()
-                .baseUrl("https://dummyjson.com/")
+                .baseUrl(endpointProduct)
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(okHttpClientBuilder().build())
